8ac4bcfa7867be2b97702e239be83253bc42e9dd
muerte20 <massan.d@yandex.ru>
2024-02-19 00:50:35 +0300

    MyApp

---
+++ app/src/main/java/com/example/helloapp/DisplayMessageActivity.java
@@ -0,0 +1,19 @@
+package com.example.helloapp;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
+    }
+}
